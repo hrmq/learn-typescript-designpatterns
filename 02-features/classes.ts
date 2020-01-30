f2d58c88ad13b83300796331db3730c8f8c17470
Administrator <h.rahimi.moqadam@gmail.com>
2020-01-30 14:01:06 -0500

    fields with inheritance

--- 02-features/classes.ts
+++ 02-features/classes.ts
@@ -1,10 +1,16 @@
 class Vehicle {
+  constructor(public color: string) {
+    this.color = color;
+  }
   protected honk(): void {
     console.log('Beep');
   }
 }
 
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log('Vroom');
   }
@@ -15,5 +21,5 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'red');
 car.startDrivingProcess();
