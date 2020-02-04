69b38d280fdb52bef5bcb087ab4c3ced10f505dc
Administrator <h.rahimi.moqadam@gmail.com>
2020-02-04 15:21:58 -0500

    add map class

---
+++ 03-maps/src/CustomMap.ts
@@ -0,0 +1,33 @@
+// Instructions to every other class
+// on  how they can be an arguemnt of 'addMarker'
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+  }
+}
