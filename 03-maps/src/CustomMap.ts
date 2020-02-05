68e603dbc6c1db1c916a586745012b2012ac9861
Administrator <h.rahimi.moqadam@gmail.com>
2020-02-05 13:04:28 -0500

    update custom map definition

--- 03-maps/src/CustomMap.ts
+++ 03-maps/src/CustomMap.ts
@@ -6,6 +6,8 @@ interface Mappable {
     lat: number;
     lng: number;
   };
+
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -32,7 +34,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi There'
+        content: mappable.markerContent()
       });
       infoWindow.open(this.googleMap, marker);
     });
