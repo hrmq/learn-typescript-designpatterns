36ba0d80ffda7a9d472a20f034f8971246de48f1
hrmq <h.rahimi.moqadam@gmail.com>
2020-03-28 17:20:56 -0400

    add trigger method to the user class

--- 06-web/index.ts
+++ 06-web/index.ts
@@ -1,6 +1,17 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'MyName', age: 20 });
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
 
-console.log(user);
+user.on('change', () => {
+  console.log('Change #2');
+});
+
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+user.trigger('change');
+user.trigger('save');
