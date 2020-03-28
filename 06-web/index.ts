daba1ca7f4bd36e7b2d06c0f1d48798a3ac6ab9f
hrmq <h.rahimi.moqadam@gmail.com>
2020-03-28 17:07:18 -0400

    add event registration method to the user class

--- 06-web/index.ts
+++ 06-web/index.ts
@@ -1,7 +1,6 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'MyName', age: 20 });
-user.set({ name: 'newname' });
+user.on('change', () => {});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
