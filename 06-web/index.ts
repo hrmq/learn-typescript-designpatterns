2121c449beb055929000ab4ef5f27628d4c47eb2
hrmq <h.rahimi.moqadam@gmail.com>
2020-03-31 21:42:05 -0400

    setting data while triggering

--- 06-web/index.ts
+++ 06-web/index.ts
@@ -1,3 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: 'new 2' });
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ age: 12 });
