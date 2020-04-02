80c5ee99d356a172963fc8029f4bcb6e896d3498
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-01 20:43:15 -0400

    final user refactor

---
+++ 06-web/models/Model.ts
@@ -0,0 +1,59 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+
+interface ModeAttributes<T> {
+  get<K extends keyof T>(key: K): T[K];
+  set(value: T): void;
+  getAll(): T;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModeAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
+
+  set(update: T) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save() {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => this.trigger('error'));
+  }
+}
