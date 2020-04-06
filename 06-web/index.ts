f2d2c6425bd4fb3a2b970b9db4c904b641aa5f65
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-06 19:28:32 -0400

    collection view implementation

--- 06-web/index.ts
+++ 06-web/index.ts
@@ -1,13 +1,20 @@
-import { UserEdit } from './Views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './Views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'Name', age: 20 });
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
