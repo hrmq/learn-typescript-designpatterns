1a343771859a529614d2a6fb43077765e779c09d
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-10 13:53:28 -0400

    fixing routes

--- 07-server/src/controllers/LoginController.ts
+++ 07-server/src/controllers/LoginController.ts
@@ -35,4 +35,10 @@ class LoginController {
       res.send('please provide email address');
     }
   }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect('/');
+  }
 }
