58571cb99046886ecba3b3555113ced1fee1afb8
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-10 13:43:09 -0400

    add body validator and refactor login post

--- 07-server/src/controllers/LoginController.ts
+++ 07-server/src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { get, controller, use } from './decorators';
+import { get, controller, use, bodyValidator, post } from './decorators';
 
 @controller('/auth')
 class LoginController {
@@ -18,5 +18,21 @@ class LoginController {
               <button>Submit</button>
           </form>
         `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      // mark this person as logged in
+      req.session = { loggedIn: true };
+
+      // redirect them to the root route
+      res.redirect('/');
+    } else {
+      res.send('please provide email address');
+    }
   }
 }
--- 07-server/src/controllers/decorators/controller.ts
+++ 07-server/src/controllers/decorators/controller.ts
@@ -2,6 +2,25 @@ import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+
+function bodyValidators(keys: string[]): RequestHandler {
+  return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid Request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send(`Missing Property ${key}`);
+        return;
+      }
+    }
+
+    next();
+  };
+}
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -20,12 +39,23 @@ export function controller(routePrefix: string) {
         key
       );
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       const middlewares =
         Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
         [];
 
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
